13e324e3d751883ee9be557c196b1bfb3bdc97fb
James <jameserne9@gmail.com>
2023-05-09 15:27:42 +0800

    added methods

--- src/main/java/com/champets/fureverhome/user/service/UserService.java
+++ src/main/java/com/champets/fureverhome/user/service/UserService.java
@@ -1,11 +1,14 @@
 package com.champets.fureverhome.user.service;
 
-
+import com.champets.fureverhome.user.model.User;
 import com.champets.fureverhome.user.model.dto.UserDto;
 
 import java.util.List;
 
 public interface UserService {
     List<UserDto> findAllUsers();
+
+    User saveUser(User user);
+    User findById(Long id);
 }
 
