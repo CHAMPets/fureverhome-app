67c51ab31dc52ed71341d8c0353886561d686d63
troy9393 <troymikaelesguerra@yahoo.com>
2023-05-09 10:12:07 +0800

    Initial commit

---
+++ src/main/java/com/champstart/fureverhome/FureverhomeApplication.java
@@ -0,0 +1,13 @@
+package com.champstart.fureverhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FureverhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FureverhomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/champstart/fureverhome/FureverhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.champstart.fureverhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FureverhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
