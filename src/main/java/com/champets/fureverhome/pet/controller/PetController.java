727400250de7d85458c670929bbbc8b6a1bfeb79
jonnliwagg <jonsoooww@gmail.com>
2023-05-09 14:42:48 +0800

    Added services and implementation of pet entity.

---
+++ src/main/java/com/champets/fureverhome/pet/controller/PetController.java
@@ -0,0 +1,55 @@
+package com.champets.fureverhome.pet.controller;
+
+import com.champets.fureverhome.pet.model.dto.PetDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import com.champets.fureverhome.pet.service.PetService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import com.champets.fureverhome.pet.model.Pet;
+import java.util.List;
+
+@Controller
+public class PetController {
+    private final PetService petService;
+
+    @Autowired
+    public PetController(PetService petService) {
+
+        this.petService = petService;
+    }
+
+    @GetMapping("/pets")
+    public String listPets(Model model){
+        List<PetDto> pets = petService.findAllPets();
+        model.addAttribute("pets", pets);
+        return "pet-list";
+    }
+
+    @GetMapping("pets/new")
+    public String createPetForm(Model model){
+        Pet pet = new Pet();
+        model.addAttribute("pet", pet);
+        return "pet-create";
+    }
+    @PostMapping("pets/new")
+    public String savePet(@ModelAttribute("pet") Pet pet){
+
+        petService.savePet(pet);
+        return "redirect:/pets";
+    }
+
+    @GetMapping("/pets/{petId}/edit")
+    public String editPetForm(@PathVariable("petId") Long petId, Model model){
+        Pet pet = petService.findPetById(petId);
+        model.addAttribute("pet", pet);
+        return "pets-edit";
+    }
+
+    @PostMapping("pets/{petId}/edit")
+    public String updatePet(@PathVariable("petId") Long petId, @ModelAttribute("club") Pet pet){
+        pet.setId(petId);
+        petService.updatePet(pet);
+        return "redirect:/pets";
+    }
+}
---
+++ src/main/java/com/champets/fureverhome/pet/model/dto/PetDto.java
@@ -0,0 +1,30 @@
+package com.champets.fureverhome.pet.model.dto;
+
+import com.champets.fureverhome.pet.enums.BodySize;
+import com.champets.fureverhome.pet.enums.Gender;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class PetDto {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private Gender gender;
+    private BodySize bodySize;
+    private LocalDate rescueDate;
+    private String imagePath;
+    private String description;
+    private Boolean isSterilized;
+    private Integer applicationLimit;
+    private Integer applicationCounter;
+    private LocalDate lastDateModified;
+    private String createdBy;
+    private String lastModifiedBy;
+    private LocalDate createdDate;
+
+}
